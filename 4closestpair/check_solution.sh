d5dcbae624f09a6f2c061fd1cfaa6304d51124d6
LarsAstrom <astrom.lars@telia.com>
2019-02-24 14:51:50 +0100

    lab 4 finished

---
+++ 4closestpair/check_solution.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# make exacutable: chmod +x check_solution.sh
+# run: ./check_solution.sh pypy A.py
+# or
+# ./check_solution.sh java solution
+# ./check_solution.sh ./a.out
+
+for f in data/**/*.in; do
+    echo $f
+    pre=${f%.in}
+    out=$pre.out
+    ans=$pre.ans
+    $* < $f > $out
+    DIFF=$(diff $ans $out)
+    if [ "$DIFF" == "" ]
+    then 
+        echo Correct!
+    else
+        echo $f Incorrect!
+        exit 1
+    fi
+done
